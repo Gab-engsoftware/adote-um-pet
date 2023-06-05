e183f979105d745e5e018b89bee64c423c89e9a2
Geovane Roque <geovaneburi11@hotmail.com>
2023-06-05 17:43:40 -0300

    Foi alterado o model principal de pets para que sirva apenas como um model para a criação utilizando Builder no modelo GOF

---
+++ backend/src/main/java/com/gabriel/models/PetShop.java
@@ -0,0 +1,14 @@
+package com.gabriel.models;
+
+//Director: Comanda o processo de construção usando o builder
+class PetShop {
+	private PetBuilder petBuilder;
+	
+	public PetShop(PetBuilder petBuilder) {
+		this.petBuilder = petBuilder;
+	}
+	
+	public Pet definirPet() {
+		return petBuilder.getPet();
+	}
+}
